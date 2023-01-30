1118b98e9be5d08ffd1e1fef95e70fa6e70a26b4
gilblum <46445472+GilBlum@users.noreply.github.com>
2023-01-30 19:13:40 +0200

    Create Python1.py
    Main

---
+++ Python1.py
@@ -0,0 +1,17 @@
+# importing pandas package
+import pandas as pd
+
+# making data frame from csv file
+data = pd.read_csv("employees.csv")
+
+# generating one row
+rows = data.sample(frac =.25)
+
+# checking if sample is 0.25 times data or not
+
+if (0.25*(len(data))== len(rows)):
+	print( "Cool")
+	print(len(data), len(rows))
+
+# display
+rows
